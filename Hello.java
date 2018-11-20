0892e5cf0962f91b13f54e033ad25f8e2fed507d
sampath77 <35134223+sampath77@users.noreply.github.com>
2018-11-20 01:00:40 -0800

    Add files via upload
    my projects

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+
+
+
+    char[]ch=new char[3];
+
+    ch[0]='s';
+    ch[1]='a';
+    ch[2]='m';
+
+    for (int a=ch.length-1;a>=0;a--){
+        System.out.println(ch[a]);
+    }
+}
+}
---
+++ Switch.java
@@ -0,0 +1,41 @@
+public class Switch {
+
+    public static void main(String[] args) {
+
+
+        switch (args[0]){
+
+            case "1":
+                System.out.println("mon");
+                break;
+
+                case "2":
+
+                    System.out.println("tue");
+                    break;
+            case "3":
+                System.out.println("wed");
+                break;
+
+                case "4":
+                    System.out.println("thu");
+                    break;
+            case "5":
+                System.out.println("fri");
+                break;
+
+                case "6":
+                    System.out.println("sat");
+                    break;
+
+            case "7":
+                System.out.println("sun");
+                break;
+
+        }
+
+
+        //System.out.println(args[0]);
+
+    }
+}
